3161c1672436bab7549f88af8541162d378328b5
Jorge Marques <marquesfilho.j.p@gmail.com>
2026-07-17 11:08:39 -0300

    Atualization

---
+++ R/app_45.R
@@ -0,0 +1,18 @@
+#' APP > 45°
+#'
+#'\strong APP > 45°
+#'
+#'@param x Entrada do arquivo matricial MDE.
+#'
+#'@examples
+#'library(terra)
+#'library(rgeoamb)
+#'dem <- terra::rast(system.file('ex/elev.tif, package = 'terra'))
+#'app <- rgeoamb::app_45(dem)
+#'plot(app)
+#'@export
+app_45 <- function(x){
+  declividade <- terra::terrain(x, 'slope', neighbors = 8, unit = 'degrees')
+  app <- terra::ifel(declividade >= 45, 1,0)
+  return(app)
+}
---
+++ R/area_uso_restrito.R
@@ -0,0 +1,18 @@
+#' Área de Uso Restrito
+#'
+#'\strong Área de Uso Restrito
+#'
+#'@param x Entrada do arquivo matricial MDE.
+#'
+#'@examples
+#'library(terra)
+#'library(rgeoamb)
+#'dem <- terra::rast(system.file('ex/elev.tif, package = 'terra'))
+#'uso_restrito <- rgeoamb::area_uso_restrito(dem)
+#'plot(uso_restrito)
+#'@export
+area_uso_restrito <- function(x){
+  declividade <- terra::terrain(x, 'slope', neighbors = 8, unit = 'degrees')
+  area <- terra::ifel(declividade >= 25 & declividade <= 45, 1, 0)
+  return(area)
+}
